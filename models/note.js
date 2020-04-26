9c6ba8115832d12c3a5c04049d39da35e1340cfb
Shahid <shahid.sarker@gmail.com>
2020-04-26 18:46:29 -0400

    notes model refactor

--- models/note.js
+++ models/note.js
@@ -1,15 +1,5 @@
-require("dotenv").config();
 const mongoose = require("mongoose");
 mongoose.set("useFindAndModify", false);
-
-const url = process.env.MONGODB_URI;
-
-console.log("connecting to", url);
-
-mongoose
-  .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((result) => console.log("connected to MongoDB"))
-  .catch((err) => console.log("error connecting to MongoDB", err.message));
 
 const noteSchema = new mongoose.Schema({
   content: { type: String, minlength: 5, required: true },
