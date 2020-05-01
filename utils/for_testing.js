382230185157e8a4af4c4ccb484d6f9acef7cfc5
Shahid <shahid.sarker@gmail.com>
2020-04-30 21:05:50 -0400

    write average tests and refactor error handling

--- utils/for_testing.js
+++ utils/for_testing.js
@@ -18,7 +18,7 @@ const average = (array) => {
    */
   const reducer = (sum, item) => sum + item;
 
-  return array.reduce(reducer, 0) / array.length;
+  return array.length === 0 ? 0 : array.reduce(reducer, 0) / array.length;
 };
 
 module.exports = { palindrome, average };
