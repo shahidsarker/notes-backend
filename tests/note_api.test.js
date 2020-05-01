4e3a816b5f423608eb4e66dc7dbb19ea4620742d
Shahid <shahid.sarker@gmail.com>
2020-04-30 22:22:37 -0400

    Initialize test database and unit tests

---
+++ tests/note_api.test.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../app");
+const api = supertest(app);
+const Note = require("../models/note");
+
+const initialNotes = [
+  {
+    content: "HTML is easy",
+    important: false,
+    date: Date.now(),
+  },
+  {
+    content: "Browser can execute only JavaScript",
+    important: true,
+    date: Date.now(),
+  },
+];
+
+beforeEach(async () => {
+  await Note.deleteMany({});
+
+  let noteObject = new Note(initialNotes[0]);
+  await noteObject.save();
+
+  noteObject = new Note(initialNotes[1]);
+  await noteObject.save();
+});
+
+test("notes are returned as json", async () => {
+  await api
+    .get("/api/notes")
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+});
+
+test("all notes are returned", async () => {
+  const response = await api.get("/api/notes");
+
+  expect(response.body).toHaveLength(initialNotes.length);
+});
+
+test("a specific notes is within the returned notes", async () => {
+  const response = await api.get("/api/notes");
+
+  const contents = response.body.map((r) => r.content);
+
+  expect(contents).toContain("Browser can execute only JavaScript");
+});
+
+afterAll(() => mongoose.connection.close());
