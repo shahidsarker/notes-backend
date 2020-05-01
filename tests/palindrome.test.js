a6b1633fdc96d350b3254f0bf7b1b53eb14ae702
Shahid <shahid.sarker@gmail.com>
2020-04-30 21:00:26 -0400

    palindrome tests

--- tests/palindrome.test.js
+++ tests/palindrome.test.js
@@ -5,3 +5,15 @@ test("palindrome of a", () => {
 
   expect(result).toBe("a");
 });
+
+test("palindrome of react", () => {
+  const result = palindrome("react");
+
+  expect(result).toBe("tcaer");
+});
+
+test("palindrome of releveler", () => {
+  const result = palindrome("releveler");
+
+  expect(result).toBe("releveler");
+});
